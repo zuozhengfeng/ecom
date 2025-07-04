26fd8ee4e6b2d6e50abfb16b4a0e26c096cf6185
zuozhengfeng <zuozhengfeng@xiaoluyy.com>
2025-07-04 14:09:41 +0800

    feat:禁用CSRF，因为DEMO没有提供更丰富的支持

--- src/main/java/com/yabobaozb/ecom/admin/config/SecurityConfig.java
+++ src/main/java/com/yabobaozb/ecom/admin/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,7 +24,8 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests((requests) -> requests
                     .requestMatchers("/actuator/health", "/actuator/**").hasRole("ADMIN")
                     .anyRequest().permitAll()
-            ).httpBasic(Customizer.withDefaults()); // 启用基本认证
+            ).httpBasic(Customizer.withDefaults())  // 启用基本认证
+        .csrf(AbstractHttpConfigurer::disable);     // 不使用CSRF
 
         return httpSecurity.build();
     }
